2b34dd6d3619057ccd69c864b668de5d7c1e1a1a
Deolia <ayushi.deolia@capgemini.com>
2022-03-24 16:30:04 +0530

    first Commit

---
+++ src/main/java/com/multiplex/Multiplexbooksystem/MultiplexbooksystemApplication.java
@@ -0,0 +1,13 @@
+package com.multiplex.Multiplexbooksystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultiplexbooksystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MultiplexbooksystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/multiplex/Multiplexbooksystem/MultiplexbooksystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.multiplex.Multiplexbooksystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultiplexbooksystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
